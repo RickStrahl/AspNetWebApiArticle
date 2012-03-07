e82959e8cef4ee6991bc93c9c8040ea90558ce49
Rick Strahl <rstrahl@west-wind.com>
2012-03-06 14:19:52 -1000

    Initial Checkin

---
+++ AspNetWebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetWebApi.Global" Language="C#" %>
---
+++ AspNetWebApi/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+using System.Web.Routing;
+using System.Web.Http;
+
+namespace AspNetWebApi
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+          // 
+          RouteTable.Routes.MapHttpRoute(
+              name: "StockApi",
+              routeTemplate: "api/{controller}/{id}",
+              defaults: new { id = RouteParameter.Optional }
+          );
+          RouteTable.Routes.MapHttpRoute(
+              name: "StockApi",
+              routeTemplate: "api/{controller}/{action}/{id}",
+              defaults: new { id = RouteParameter.Optional }
+          );
+        }
+
+   }
+}
