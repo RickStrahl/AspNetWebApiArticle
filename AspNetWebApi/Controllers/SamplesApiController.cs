0b0d7fc38914fb5bd8ea7bf754b858dabe45e2a7
Rick Strahl <rstrahl@west-wind.com>
2012-09-04 12:53:16 -1000

    Add POST additional examples

--- AspNetWebApi/Controllers/SamplesApiController.cs
+++ AspNetWebApi/Controllers/SamplesApiController.cs
@@ -267,6 +267,30 @@ namespace AspNetWebApi
             };
         }
 
+        [HttpPost]
+        public JObject PostAlbumJObject(JObject jAlbum)
+        {
+            // dynamic input from inbound JSON
+            dynamic album = jAlbum;
+
+            // create a new JSON object to write out
+            dynamic newAlbum = new JObject();
+
+            // Create properties on the new instance
+            // with values from the first
+            newAlbum.AlbumName = album.AlbumName + " New";
+            newAlbum.NewProperty = "something new";
+            newAlbum.Songs = new JArray();
+    
+            foreach (dynamic song in album.Songs)
+            {
+                song.SongName = song.SongName + " New"; 
+                newAlbum.Songs.Add(song);                
+            }
+            
+            return newAlbum;
+        }
+
     }
 
     public class LoginData
